c7952d726f93d9b4c6ff91710b04849112583a87
lllggghhhaaa <lgha1423@outlook.com>
2023-06-16 23:06:03 +0000

    Cli application implemented

--- WaxMapArt.Console/CliOptions.cs
+++ WaxMapArt.Console/CliOptions.cs
@@ -1,6 +1,5 @@
 using CommandLine;
 using WaxMapArt;
-using WaxMapArt.ImageProcessing;
 using WaxMapArt.ImageProcessing.Dithering;
 
 public class CliOptions
@@ -11,10 +10,14 @@ public class CliOptions
     public string OutputPath { get; set; }
     [Option('p', "preview", Required = true, HelpText = "Preview image path")]
     public string PreviewPath { get; set; }
+    [Option('c', "palette", Required = true)]
+    public string Palette { get; set; }
     [Option('m', "method", Default = ComparisonMethod.Cie76)]
     public ComparisonMethod ColorComparisonMethod { get; set; }
     [Option('d', "dithering", Default = DitheringType.None)]
     public DitheringType Dithering { get; set; }
+    [Option('s', "staircase", Default = true)]
+    public bool Staircase { get; set;}
     [Option('w', "width", Default = 1)]
     public int Width { get; set; }
     [Option('h', "height", Default = 1)]
--- WaxMapArt.Console/Program.cs
+++ WaxMapArt.Console/Program.cs
@@ -1,2 +1,45 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using CommandLine;
+using Newtonsoft.Json;
+using SixLabors.ImageSharp;
+using SixLabors.ImageSharp.PixelFormats;
+using WaxMapArt;
+
+Parser.Default.ParseArguments<CliOptions>(args).WithParsed(o =>
+{
+    Palette palette = JsonConvert.DeserializeObject<Palette>(File.ReadAllText(o.Palette));
+    Image<Rgb24> image = Image.Load<Rgb24>(o.ImagePath);
+
+    Generator generator = new Generator(palette)
+    {
+        Method = o.ColorComparisonMethod,
+        Dithering = o.Dithering,
+        MapSize = new WaxSize(o.Width, o.Height),
+        OutputSize = new WaxSize(512, 512)
+    };
+
+    GeneratorOutput generatorOutput = o.Staircase ? generator.GenerateStaircase(image) : generator.GenerateFlat(image);
+
+    generatorOutput.Image.SaveAsPng(o.PreviewPath);
+
+    foreach (var (mapId, count) in generatorOutput.BlockList)
+    {
+        int packs = count / 64;
+        int rem = count % 64;
+        double shulkers = Math.Truncate(packs / 27f * 100) / 100;
+        
+        string packCount = packs > 0 ? $"{packs} packs + {rem}" : count.ToString();
+        string id = palette.Colors[mapId.ToString()].BlockId;
+        
+        Console.WriteLine(id);
+        Console.WriteLine($"Shulker box: {shulkers}");
+        Console.WriteLine(packCount);
+        Console.WriteLine();
+    }
+
+    Stream stream = NbtGenerator.Generate(generatorOutput.Blocks);
+    using FileStream fs = File.Create(o.OutputPath);
+    stream.CopyTo(fs);
+
+    stream.Close();
+    fs.Close();
+});
