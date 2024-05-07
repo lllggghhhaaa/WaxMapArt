8c246db196641e0cd335c4a2de49158f8b2757ec
lllggghhhaaa <lgha1423@outlook.com>
2024-05-07 12:35:36 -0300

    MAUI project added

---
+++ WaxMapArt.Maui/App.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace WaxMapArt.Maui;
+
+public partial class App : Application
+{
+    public App()
+    {
+        InitializeComponent();
+
+        MainPage = new MainPage();
+    }
+}
---
+++ WaxMapArt.Maui/MainPage.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace WaxMapArt.Maui;
+
+public partial class MainPage : ContentPage
+{
+    public MainPage()
+    {
+        InitializeComponent();
+    }
+}
