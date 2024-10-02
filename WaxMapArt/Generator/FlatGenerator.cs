cf145fc6dbfc1dabfde1022a7c58cac5f7eadcf2
lllggghhhaaa <lgha1423@outlook.com>
2024-10-02 15:48:56 -0300

    Added flat map generator
    Signed-off-by: lllggghhhaaa <lgha1423@outlook.com>

---
+++ WaxMapArt/Generator/FlatGenerator.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections.Concurrent;
+using SixLabors.ImageSharp;
+using SixLabors.ImageSharp.PixelFormats;
+using WaxMapArt.Entities;
+using WaxMapArt.Utils;
+
+namespace WaxMapArt.Generator;
+
+public class FlatGenerator : IGenerator
+{
+    public GeneratorOutput Generate(Image<Rgb24> image, Palette palette)
+    {
+        var blocks = new ConcurrentBag<BlockInfo>();
+
+        Parallel.For(0, image.Width, x =>
+        {
+            for (var y = 0; y < image.Height; y++)
+            {
+                var pixel = image[x, y];
+
+                var blockInfo = palette.Colors.First(color => ColorUtils.MapIdToInfo(color.MapId).Color.Equals(pixel));
+                
+                blocks.Add(new BlockInfo
+                {
+                    X = x,
+                    Y = 0,
+                    Z = y,
+                    Id = blockInfo.Id,
+                    Properties = blockInfo.Properties
+                });
+            }
+        });
+        
+        return new GeneratorOutput(blocks.ToArray());
+    }
+}
