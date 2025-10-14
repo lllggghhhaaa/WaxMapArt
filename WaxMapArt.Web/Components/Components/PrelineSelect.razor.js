e2538df4a82a4309b8952d3d8c575af1e13b596e
lllggghhhaaa <lgha1423@outlook.com>
2025-10-14 13:01:17 -0300

    Refactored select to support custom option template

---
+++ WaxMapArt.Web/Components/Components/PrelineSelect.razor.js
@@ -0,0 +1,13 @@
+﻿export function initSelect(element, dotNetRef) {
+    const handleClickOutside = (event) => {
+        if (element && !element.contains(event.target)) {
+            dotNetRef.invokeMethodAsync('CloseFromOutside');
+        }
+    };
+
+    document.addEventListener('click', handleClickOutside);
+
+    element._cleanup = () => {
+        document.removeEventListener('click', handleClickOutside);
+    };
+}
